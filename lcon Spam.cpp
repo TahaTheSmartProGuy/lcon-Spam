08ea76a16f2791aa0b18ef01d13a68a011b52fcb
Taha! <164132872+TahaTheSmartProGuy@users.noreply.github.com>
2024-05-14 17:20:06 +0300

    Add files via upload

---
+++ lcon Spam.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+
+int main() {
+	HDC hDc = GetWindowDC(GetDesktopWindow());
+	int x = rand() % GetSystemMetrics(SM_CXSCREEN);
+	int y = rand() % GetSystemMetrics(SM_CYSCREEN);
+	while (true)
+	{
+		FreeConsole();
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		DrawIcon(hDc, x, y, LoadIcon(0, IDI_ERROR));
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		DrawIcon(hDc, x, y, LoadIcon(0, IDI_QUESTION));
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		DrawIcon(hDc, x, y, LoadIcon(0, IDI_WARNING));
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		DrawIcon(hDc, x, y, LoadIcon(0, IDI_APPLICATION));
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		Sleep(0);
+	}
+}
